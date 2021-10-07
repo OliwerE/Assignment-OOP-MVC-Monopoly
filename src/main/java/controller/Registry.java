fcc9f8b937265aeb8c3eef9bb65109aa5f412bc6
Oliwer <oe222ez@student.lnu.se>
2021-10-07 20:51:37 +0200

    Add getInteger in Console

--- src/main/java/controller/Registry.java
+++ src/main/java/controller/Registry.java
@@ -4,6 +4,8 @@ public class Registry {
   model.Registry registry = new model.Registry();
 
   public void start(view.Console c) {
-    c.printTest();
+    c.mainMenu();
+    Integer i = c.getInteger();
+    System.out.println("valde: " + i);
   }
 }
--- src/main/java/view/Console.java
+++ src/main/java/view/Console.java
@@ -1,7 +1,22 @@
 package view;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Console {
-  public void printTest() {
-    System.out.println("Hello from view!");
+  Scanner scanner = new Scanner(System.in);
+
+  public void mainMenu() {
+    System.out.println("---------- Member Registry ----------");
+    System.out.print("Verbose member list (1), compact member list (2), exit (3): ");
+  }
+
+  public Integer getInteger() {
+    try {
+      return scanner.nextInt();
+    } catch (InputMismatchException e) {
+      System.out.println(e);
+      return -1; // Error
+    }
   }
 }
